836472180ed10848a98b6f9e3f603c291cf8bbb2
DMITRY <Martyrer@users.noreply.github.com>
2017-01-23 14:23:15 +0200

    Update ErivelloSimpleHtmlDomBundle.php
    namespace changed

--- ErivelloSimpleHtmlDomBundle.php
+++ ErivelloSimpleHtmlDomBundle.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace Erivello\SimpleHtmlDomBundle;
+namespace Martyrer\SimpleHtmlDomBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class ErivelloSimpleHtmlDomBundle extends Bundle
+class MartyrerSimpleHtmlDomBundle extends Bundle
 {
 }
