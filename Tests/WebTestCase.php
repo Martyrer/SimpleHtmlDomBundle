dacbde2d9947b96511eafe4e71d1a4edac86c00b
DMITRY <Martyrer@users.noreply.github.com>
2017-01-23 14:25:15 +0200

    Update WebTestCase.php

--- Tests/WebTestCase.php
+++ Tests/WebTestCase.php
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Erivello\SimpleHtmlDomBundle\Tests;
+namespace Martyrer\SimpleHtmlDomBundle\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase as BaseWebTestCase;
 
@@ -29,7 +29,7 @@ abstract class WebTestCase extends BaseWebTestCase
     {
         require_once __DIR__.'/Fixtures/app/AppKernel.php';
 
-        return 'Erivello\SimpleHtmlDomBundle\Tests\Functional\AppKernel';
+        return 'Martyrer\SimpleHtmlDomBundle\Tests\Functional\AppKernel';
     }    
 
     protected static function createKernel(array $options = array())
